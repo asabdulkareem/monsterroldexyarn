f1dd869be3d016d9afeb120ca2a70e597cde9772
abdul kareem <as.abdulkareem@gmail.com>
2020-10-04 16:54:31 +0530

    changes for Search Box styles

--- src/Components/Search-Box/Search-Box.component.jsx
+++ src/Components/Search-Box/Search-Box.component.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './Search-Box.Styles.css';
 
 export const SearchBox = ({PlaceHolder, handleChange}) => (
-    <input 
-          type='search' 
-          placeholder={PlaceHolder} 
-          onChange={handleChange}
-        /> 
+    <input
+        className="search" 
+        type='search' 
+        placeholder={PlaceHolder} 
+        onChange={handleChange}
+    /> 
 );
