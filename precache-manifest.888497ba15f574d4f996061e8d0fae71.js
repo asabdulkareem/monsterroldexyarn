d89e3e50f745b5067ccbcf8351bcbe2ac5e21007
abdul kareem <as.abdulkareem@gmail.com>
2020-10-04 16:41:00 +0530

    Updates

---
+++ precache-manifest.888497ba15f574d4f996061e8d0fae71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f616e063b39e1ed129e7882854cf84",
+    "url": "/monsterroldexyarn/index.html"
+  },
+  {
+    "revision": "9c46ec70da6938e51024",
+    "url": "/monsterroldexyarn/static/css/main.23078317.chunk.css"
+  },
+  {
+    "revision": "3d05061415545e980b05",
+    "url": "/monsterroldexyarn/static/js/2.2d5450c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterroldexyarn/static/js/2.2d5450c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c46ec70da6938e51024",
+    "url": "/monsterroldexyarn/static/js/main.87535d74.chunk.js"
+  },
+  {
+    "revision": "1ae0543d58804c94beac",
+    "url": "/monsterroldexyarn/static/js/runtime-main.a3fa54cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsterroldexyarn/static/media/logo.5d5d9eef.svg"
+  }
+]);
